866497bde2409406a35e4a98ec124a4f91301e3f
haniii294 <148616894+haniii294@users.noreply.github.com>
2025-06-13 12:26:36 +0700

    Create loans.js

---
+++ perpustakaan-api/routes/loans.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const Loan = require('../models/loan');
+
+// CREATE loan
+router.post('/', async (req, res) => {
+  const loan = new Loan({
+    ...req.body,
+    borrowDate: new Date()
+  });
+  await loan.save();
+  res.status(201).json(loan);
+});
+
+// READ all
+router.get('/', async (req, res) => {
+  const loans = await Loan.find().populate('userId bookId');
+  res.json(loans);
+});
+
+// UPDATE (returning)
+router.put('/:id', async (req, res) => {
+  const updated = await Loan.findByIdAndUpdate(req.params.id, {
+    ...req.body,
+    returnDate: new Date(),
+    status: 'dikembalikan'
+  }, { new: true });
+  res.json(updated);
+});
+
+// DELETE
+router.delete('/:id', async (req, res) => {
+  await Loan.findByIdAndDelete(req.params.id);
+  res.sendStatus(204);
+});
+
+module.exports = router;
