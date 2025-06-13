cea96dcd89f2458d8c16b31ff8e9624b1dddce49
haniii294 <148616894+haniii294@users.noreply.github.com>
2025-06-13 12:27:54 +0700

    Create app.js

---
+++ perpustakaan-api/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const bookRoutes = require('./routes/books');
+const userRoutes = require('./routes/users');
+const loanRoutes = require('./routes/loans');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use('/api/books', bookRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/loans', loanRoutes);
+
+module.exports = app;
