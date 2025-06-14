8c4de9eee0b782a715bdb9103d7aa4de2950f959
Firdafahria23 <150696359+firdafahria@users.noreply.github.com>
2025-06-14 21:12:33 +0700

    Update rate limit

--- perpustakaan-api/app.js
+++ perpustakaan-api/app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+
 const bookRoutes = require('./routes/books');
 const userRoutes = require('./routes/users');
 const loanRoutes = require('./routes/loans');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -20,8 +24,25 @@ mongoose.connect(process.env.MONGO_URI, {
   console.error("MongoDB Connection Error:", err.message);
 });
 
+const globalLimiter = rateLimit({
+    windowMs: 10 * 1000, 
+    max: 5, 
+    message: 'Terlalu banyak permintaan dari IP ini, silakan coba lagi setelah 15 menit.', 
+    standardHeaders: true,
+    legacyHeaders: false, 
+});
+app.use(globalLimiter);
+
+const authLimiter = rateLimit({
+    windowMs: 10 * 1000, 
+    max: 3, 
+    message: 'Terlalu banyak percobaan login/register gagal dari IP ini, silakan coba lagi setelah 5 menit.', // Pesan error
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
 app.use('/api/books', bookRoutes);
-app.use('/api/users', userRoutes);
+app.use('/api/users', authLimiter , userRoutes);
 app.use('/api/loans', loanRoutes);
 
 module.exports = app;
