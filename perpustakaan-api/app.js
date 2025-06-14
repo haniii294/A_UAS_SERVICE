8bcfbedd6905b8200b114aae4ec0a9068c652b41
haniii294 <148616894+haniii294@users.noreply.github.com>
2025-06-14 18:51:16 +0700

    Update app.js

--- perpustakaan-api/app.js
+++ perpustakaan-api/app.js
@@ -9,7 +9,16 @@ const loanRoutes = require('./routes/loans');
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("MongoDB Connected (Atlas)");
+})
+.catch((err) => {
+  console.error("MongoDB Connection Error:", err.message);
+});
 
 app.use('/api/books', bookRoutes);
 app.use('/api/users', userRoutes);
